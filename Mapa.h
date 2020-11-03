5fdd4124bc905f7409d3bf52d214d6cb070517a1
CarolineMano <71740367+CarolineMano@users.noreply.github.com>
2020-11-02 21:04:47 -0300

    Update Mapa.h
    Inclusão de uma função para mostrar no console o mapa do labirinto

--- Mapa.h
+++ Mapa.h
@@ -1,6 +1,7 @@
 #ifndef Mapa_h
 #define Mapa_h
 
+#include <iostream>
 using namespace std;
 
 const char PAREDE = 'P';
@@ -16,5 +17,15 @@ struct Mapa{
         VAZIO, VAZIO, VAZIO, VAZIO, VAZIO
     };
 };
+
+void mostrarMapa(Mapa mapa, int tamanho){
+    for(int i = 0; i < tamanho; i++){
+        for(int j = 0; j < tamanho; j++){
+            cout << mapa.cenario[i][j] << "\t";
+        }
+        cout << endl;
+    }
+    cout << "**********************************" << endl;
+}
 
 #endif
