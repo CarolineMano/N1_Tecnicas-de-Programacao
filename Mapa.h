fef954762e59f128c753cc940d27caae47572cad
CarolineMano <71740367+CarolineMano@users.noreply.github.com>
2020-11-08 11:41:55 -0300

    Update Mapa.h
    Alteração da struct Mapa para poder atender aos critérios de poder ou não poder chegar ao queijo, exclusão dos atributos de entrada na struct e inclusão de um atributo de tamanho da matriz; inclusão da função para encontrar o queijo automaticamente

--- Mapa.h
+++ Mapa.h
@@ -4,6 +4,8 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
+#include <stdlib.h>
+#include "Rato.h"
 using namespace std;
 
 const char PAREDE = 'P';
@@ -13,41 +15,69 @@ const char QUEIJO = 'Q';
 struct Mapa{
     char cenario[5][5] = {
         
-        VAZIO,  PAREDE,  PAREDE, VAZIO,   VAZIO,
-        VAZIO,   VAZIO,   VAZIO, VAZIO,  PAREDE,
+        VAZIO,   VAZIO,  PAREDE, VAZIO,  QUEIJO,
         VAZIO,  PAREDE,   VAZIO, VAZIO,   VAZIO,
-        VAZIO,   VAZIO,   VAZIO, VAZIO,  PAREDE,
-        VAZIO,  PAREDE,  QUEIJO, VAZIO,  VAZIO
+        VAZIO,   VAZIO,   VAZIO, VAZIO,   VAZIO,
+        VAZIO,  PAREDE,   VAZIO, VAZIO,  PAREDE,
+        VAZIO,  PAREDE,   VAZIO, VAZIO,  VAZIO
             
     };
-    int posicEntradaX;
-    int posicEntradaY;
+    int tamanhoMatrix = 5;
 };
 
-void mostrarMapa(Mapa mapa, int tamanho){
-    for(int i = 0; i < tamanho; i++){
-        for(int j = 0; j < tamanho; j++){
-            cout << mapa.cenario[i][j] << "\t";
+void mostrarMapa(Mapa umMapa){
+    for(int i = 0; i < umMapa.tamanhoMatrix; i++){
+        for(int j = 0; j < umMapa.tamanhoMatrix; j++){
+            cout << umMapa.cenario[i][j] << "\t";
         }
         cout << endl;
     }
     cout << "**********************************" << endl;
 }
 
-void gerarEntradaAleatoria(Mapa &mapa, int tamanho){
-    bool teste = true;
-    while(teste){
+void gerarEntradaAleatoria(Mapa &umMapa, Rato &umRato){
+    bool validacaoNaMatriz = true;
+    while(validacaoNaMatriz){
         srand((int)time(0));
-        int rand1 = rand() % tamanho;
-        int rand2 = rand() % tamanho;
+        int rand1 = rand() % umMapa.tamanhoMatrix;
+        int rand2 = rand() % umMapa.tamanhoMatrix;
 
-        if(mapa.cenario[rand1][rand2] != 'P' && mapa.cenario[rand1][rand2] != 'Q'){
-            mapa.cenario[rand1][rand2] = 'E';
-            mapa.posicEntradaX = rand1;
-            mapa.posicEntradaY = rand2;
-            teste = false;
+        if(umMapa.cenario[rand1][rand2] != 'P' && umMapa.cenario[rand1][rand2] != 'Q'){
+            umMapa.cenario[rand1][rand2] = 'E';
+            umRato.posicaoX = rand1;
+            umRato.posicaoY = rand2;
+            validacaoNaMatriz = false;
         }            
     }  
+}
+
+
+bool acharCaminhoAutomaticamente(Mapa &umMapa, int posicaoX, int posicaoY, char deOndeVeio){
+    mostrarMapa(umMapa);
+    _sleep(1000);
+    if(posicaoX < 0 || posicaoX >= umMapa.tamanhoMatrix || posicaoY < 0 || posicaoY >= umMapa.tamanhoMatrix){
+        return false;
+    } else if(umMapa.cenario[posicaoX][posicaoY] == 'Q'){
+        cout << "Voce encontrou o queijo!" << endl;
+        return true;
+    } else if(umMapa.cenario[posicaoX][posicaoY] == 'P' || umMapa.cenario[posicaoX][posicaoY] == '+'){
+        return false;
+    } else{
+        umMapa.cenario[posicaoX][posicaoY] = '+';
+        if(acharCaminhoAutomaticamente(umMapa, posicaoX - 1, posicaoY, deOndeVeio) && deOndeVeio != NORTE){
+            return true;
+        } else if(acharCaminhoAutomaticamente(umMapa, posicaoX, posicaoY + 1, deOndeVeio) && deOndeVeio != LESTE){
+            return true;
+        } else if(acharCaminhoAutomaticamente(umMapa, posicaoX + 1, posicaoY, deOndeVeio) && deOndeVeio != SUL){
+            return true;
+        } else if(acharCaminhoAutomaticamente(umMapa, posicaoX, posicaoY - 1, deOndeVeio) && deOndeVeio != OESTE){
+            return true;
+        } else{
+            umMapa.cenario[posicaoX][posicaoY] = '.';
+            return false;
+        }
+        cout << "Nao foi possivel chegar ao queijo" << endl;
+    }
 }
 
 #endif
