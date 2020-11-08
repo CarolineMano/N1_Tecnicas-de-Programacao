92be3a6bd426b0932d41753e33e416c340c16a2e
CarolineMano <71740367+CarolineMano@users.noreply.github.com>
2020-11-08 11:43:59 -0300

    Update main.cpp
    Inicialização de uma variável tipo Mapa, outra tipo Rato e chamada das funções gerarEntradaAleatoria e acharCaminhoAutomaticamente, definidas na biblioteca Mapa.h

--- main.cpp
+++ main.cpp
@@ -4,6 +4,9 @@
 using namespace std;
 
 int main(){
-
+    Mapa jogo;
+    Rato rato;
+    gerarEntradaAleatoria(jogo, rato);
+    acharCaminhoAutomaticamente(jogo, rato.posicaoX, rato.posicaoY, rato.deOndeVeio);
     return 0;
 }
