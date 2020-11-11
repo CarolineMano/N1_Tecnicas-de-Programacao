3e9bfdf490195e7394dd6d0c47079332b2594eab
CarolineMano <71740367+CarolineMano@users.noreply.github.com>
2020-11-10 21:23:12 -0300

    Update Rato.h
    Exclusão de atributos e constantes não utilizados

--- Rato.h
+++ Rato.h
@@ -1,17 +1,10 @@
 #ifndef Rato_h
 #define Rato_h
 
-const char NORTE = 'N';
-const char SUL = 'S';
-const char LESTE = 'L';
-const char OESTE = 'O';
 
 struct Rato{
     int posicaoX = 0;
     int posicaoY = 0;
-    char icone = 'R';
-    bool temQueijo = false;
-    char deOndeVeio;
 };
 
 #endif
